d68b6636ddf15e1b9f42d533914463709cf8d3f6
Rameshkanuri95 <74371283+Rameshkanuri95@users.noreply.github.com>
2024-07-11 15:39:49 +0530

    movieCard component separted

--- src/components/SerachMovies.jsx
+++ src/components/SerachMovies.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-
+import MovieCard from "./MovieCard"
 const SerachMovies = () => {
   const [query, setQuery] = useState("")
   const [movies, setMovies] = useState([])
@@ -38,23 +38,7 @@ const SerachMovies = () => {
         {movies
           .filter((movie) => movie.poster_path)
           .map((movie) => (
-            <div className="card" key={movie.title}>
-              <img
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                alt={movie.title + "poster"}
-                className="card--image"
-              />
-              <div className="card--content">
-                <h3 className="card--title">{movie.title}</h3>
-                <p>
-                  <small>RELEASE DATE:{movie.release_date}</small>
-                </p>
-                <p>
-                  <small>RATING:{movie.vote_average}</small>
-                </p>
-                <p className="card_desc">{movie.overview}</p>
-              </div>
-            </div>
+            <MovieCard key={movie.id} movie={movie} />
           ))}
       </div>
     </>
